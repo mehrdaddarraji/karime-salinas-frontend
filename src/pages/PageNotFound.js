d8b0b64ccf7084d2d8b4e2a39a8475b4cb725b7d
Mehrdad Darraji <mehrdaddarraji@Mehrdads-MacBook-Pro.local>
2020-03-17 01:44:33 -0500

    landing page, about page, cpntact page, page not found

---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class PageNotFound extends Component {
+    render() {
+        return (
+            <div>
+                Oops, this page doesn't exist :(
+            </div>
+        );
+    }
+}
+
+export default PageNotFound;
