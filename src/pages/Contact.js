6d539dc1916dd373b05f89db6ca65e4354e94dba
adrianaixba <ixbaadriana@gmail.com>
2020-03-19 17:27:39 -0500

    finished form styling

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
+import Email from '../components/Email';
 
 class Contact extends Component {
     render() {
         return (
+            
+
             <div>
+                <Email />
                 Contact Page
             </div>
         );
