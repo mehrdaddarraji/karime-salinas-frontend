8ab1781a8ecbce1e49e6746a4163afda79f64064
Mehrdad Darraji <mehrdaddarraji@Mehrdads-MacBook-Pro.local>
2020-03-19 14:32:32 -0500

    navbar with logo, home, about, porfolio, contact

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,38 @@
 import React, {Component} from 'react';
+import logoImg from '../img/logooo.jpg';
 
 class Navbar extends Component {
     render() {
         return (
-            <div>
-                Navbar
-            </div>
+			<div>
+				<nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <div className="container">
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse justify-content-center" id="navbarText">
+                            <img className="navbar-brand mr-5" src={logoImg} height="150" width="150" />
+                            <ul className="navbar-nav align-items-center ml-5">
+                                {/* <li className="nav-item" href="/">
+                                    <img className="navbar-brand" src={logoImg} height="200" width="200" />
+                                </li> */}
+                                <li className="nav-item px-4">
+                                    <a className="nav-link" href="/">Home</a>
+                                </li>
+                                <li className="nav-item px-4">
+                                    <a className="nav-link" href="/about">About</a>
+                                </li>
+                                <li className="nav-item px-4">
+                                    <a className="nav-link" href="/portfolio">Portfolio</a>
+                                </li>
+                                <li className="nav-item px-4">
+                                    <a className="nav-link" href="/contact">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+			</div>
         );
     }
 }
