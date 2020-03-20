7de8075ee4e747bd3b833f17d3c1197b29acb5a0
Mehrdad Darraji <mehrdaddarraji@Mehrdads-MacBook-Pro.local>
2020-03-19 19:31:05 -0500

    some about page styling

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
+import logoImg from '../img/logooo.jpg';
 
 class About extends Component {
     render() {
         return (
-            <div>
-                About Page
+            <div className="container">
+                <div className="row justify-content-center my-4">
+                    <h1>About Us</h1>
+                </div>
+                <div className="row justify-content-center">
+                    <h1>&mdash;</h1>
+                </div>
+                <div className="row justify-content-center mt-4">
+                    <img src={logoImg} alt="Kari Sali" height="500" width="500" />
+                </div>
+                <div className="row justify-content-center mt-4">
+                    <h4><em>Karime Salinas</em></h4>
+                </div>
             </div>
         );
     }
