6f89a0b949891c364ae0298d53b5dc0d1af62897
Mehrdad Darraji <mehrdaddarraji@Mehrdads-MacBook-Pro.local>
2020-03-22 01:23:39 -0500

    finish about page design

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,7 @@ class About extends Component {
     render() {
         return (
             <div>
-                <div className="container">
+                <div className="container text-secondary">
                     <div className="row justify-content-center my-4">
                         <h1>About Us</h1>
                     </div>
@@ -29,7 +29,7 @@ class About extends Component {
                             text text text text text text text text text text</p>
                     </div>
                 </div>
-                <div className="container my-5">
+                <div className="container my-5 text-secondary">
                     <br></br>
                     <div className="row justify-content-center">
                         <h1>SERVICES</h1>
@@ -37,28 +37,31 @@ class About extends Component {
                     <div className="row justify-content-center mt-4">
                         <div className="col">
                             <div className="card border-0" style={{width: "18rem"}}>
-                                <img className="card-img-top rounded" src="..." alt="Card designs" />
+                                <img className="card-img-top rounded-circle" src={logoImg} alt="Card designs" />
                                 <div className="card-body">
-                                    <h5 className="card-title">Designs</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <h5 className="card-title text-center">Designs</h5>
+                                    <h5 className="text-center">&mdash;&mdash;&mdash;</h5>
+                                    <p className="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                 </div>
                             </div>
                         </div>
                         <div className="col">
                             <div className="card border-0" style={{width: "18rem"}}>
-                                <img className="card-img-top rounded" src="..." alt="Card logos" />
+                                <img className="card-img-top rounded-circle" src={logoImg} alt="Card logos" />
                                 <div className="card-body">
-                                    <h5 className="card-title">Logos</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <h5 className="card-title text-center">Logos</h5>
+                                    <h5 className="text-center">&mdash;&mdash;&mdash;</h5>
+                                    <p className="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                 </div>
                             </div>
                         </div>
                         <div className="col">
                             <div className="card border-0" style={{width: "18rem"}}>
-                                <img className="card-img-top rounded" src="..." alt="Card t-shirts" />
+                                <img className="card-img-top rounded-circle" src={logoImg} alt="Card t-shirts" />
                                 <div className="card-body">
-                                    <h5 className="card-title">T-Shirts</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <h5 className="card-title text-center">T-Shirts</h5>
+                                    <h5 className="text-center">&mdash;&mdash;&mdash;</h5>
+                                    <p className="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                 </div>
                             </div>
                         </div>
