6fdb5b9e26b8b625ee79a54e0669f285073bd58a
adrianaixba <ixbaadriana@gmail.com>
2020-03-26 13:43:19 -0500

    finished main format of portfolio pages, added works, need text styling & think about works with more than 2 supporting images

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -23,21 +23,21 @@ class Portfolio extends Component {
                     <Link to='/portfolio/yruama'>
                         <img src={conchaImg} className="img-fluid mb-4" alt="Conchita"></img>
                     </Link>
-                    <p>Yruama T-Shirt Design</p>
+                    <p style={{color:'gray'}}>Yruama T-Shirt Design</p>
                 </div>
 
                 <div style= {styles.image} key='key2' className="col-lg-4 col-md-6 mb-4">
                     <Link to='/portfolio/hindsvinyl'>
                         <img src={vinylImg} className="img-fluid mb-4" alt="Vinyl"></img>
                     </Link>
-                    <p>Hinds Vinyl Cover Design</p>
+                    <p style={{color:'gray'}}>Hinds Vinyl Cover Design</p>
                 </div>
 
                 <div style= {styles.image} key='key3' className="col-lg-4 col-md-6 mb-4">
                     <Link to='/portfolio/tamagochi'>
                         <img src={tamaImg} className="img-fluid mb-4" alt="Tama"></img>
                     </Link>
-                    <p>Tamagotchi Sticker Design</p>
+                    <p style={{color:'gray'}}>Tamagotchi Sticker Design</p>
                 </div>
 
             </div>
