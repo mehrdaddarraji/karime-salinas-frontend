f0fc1dabbf3d9c0486ff261866d034db4efd183e
Mehrdad Darraji <mehrdaddarraji@Mehrdads-MacBook-Pro.local>
2020-03-26 18:44:08 -0500

    adding basic landing page

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,10 +1,23 @@
 import React, {Component} from 'react';
+import conchitaImg from '../img/conchita.jpg';
+import mockupImg from '../img/Mockup.jpg';
+import tamaStickerImg from '../img/tama_sticker.jpg';
 
 class LandingPage extends Component {
     render() {
         return (
-            <div>
-                Landing Page
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-4 col-md-6 mb-4">
+                        <img className="img-fluid" src={conchitaImg} alt="conchita" />
+                    </div>
+                    <div className="col-lg-4 col-md-6 mb-4">
+                        <img className="img-fluid" src={mockupImg} alt="mockup" />
+                    </div>
+                    <div className="col-lg-4 col-md-6 mb-4">
+                        <img className="img-fluid" src={tamaStickerImg} alt="tama sticker" />
+                    </div>
+                </div>
             </div>
         );
     }
