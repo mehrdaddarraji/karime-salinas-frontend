e4586cc4db0ef6e94aaec669ac0afaa7ccef1091
Mehrdad Darraji <mehrdad_da@yahoo.com>
2020-04-01 00:24:39 -0500

    centered the navbar when collapsed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,11 +13,11 @@ class Navbar extends Component {
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse justify-content-center" id="navbarText">
-                            <img className="navbar-brand mr-5" src={logoImg} height="150" width="150" alt="Kari Sali Designs" />
-                            <ul className="navbar-nav align-items-center ml-5">
-                                {/* <li className="nav-item" href="/">
-                                    <img className="navbar-brand" src={logoImg} height="200" width="200" />
-                                </li> */}
+                            {/* <img className="navbar-brand mr-5" src={logoImg} height="150" width="150" alt="Kari Sali Designs" /> */}
+                            <ul className="navbar-nav align-items-center">
+                                <li className="nav-item px-4">
+                                    <img className="navbar-brand" src={logoImg} height="150" width="150" alt="Kari Sali Designs" />
+                                </li>
                                 <li className={`nav-item px-4 ${path === "/" ? 'active' : ''}`}>
                                     <a className="nav-link" href="/">Home</a>
                                 </li>
